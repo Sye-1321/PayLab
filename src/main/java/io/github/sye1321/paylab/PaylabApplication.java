19670545a6d269c54f18163120ec3a389f64fc2c
sye <syevidaljr@gmail.com>
2026-09-22 10:21:39 +0300

    build: initialize Spring Boot application

---
+++ src/main/java/io/github/sye1321/paylab/PaylabApplication.java
@@ -0,0 +1,12 @@
+package io.github.sye1321.paylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaylabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaylabApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/sye1321/paylab/PaylabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sye1321.paylab;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PaylabApplicationTests {
+
+    @Test
+    void applicationSkeletonExists() {
+        assertNotNull(new PaylabApplication());
+    }
+}
